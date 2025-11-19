f61cf5585a469f6136e90a3060f6a0504cb5068e
Alina <zikibikk@gmail.com>
2025-11-19 15:17:48 +0300

    Initial Commit

---
+++ TaskTrackerBikkinina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskTrackerBikkinina
+//
+//  Created by Alina Bikkinina on 19.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TaskTrackerBikkininaTests/TaskTrackerBikkininaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerBikkininaTests.swift
+//  TaskTrackerBikkininaTests
+//
+//  Created by Alina Bikkinina on 19.11.2025.
+//
+
+import Testing
+@testable import TaskTrackerBikkinina
+
+struct TaskTrackerBikkininaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
